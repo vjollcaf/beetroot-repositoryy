fffa7dba0ae0cdb208f4a6e9f12067a348c1ba76
Vjollcaf <franguvjollca@gmail.com>
2022-02-16 11:04:25 +0100

    Javascript Objects homework

---
+++ objects-hw/assets/js/main.js
@@ -0,0 +1,10 @@
+let car = (manufacturer, model,year,speed){
+	this.manufacturer = 'Ford',
+	this.model = 'Mustang',
+	this.year =1965;
+	this.speed = 138;
+
+	displayInfo: {
+		console.log(car);
+	}
+}
